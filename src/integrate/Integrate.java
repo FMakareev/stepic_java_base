863a5e58a9f6a4af6af86aa3bb4cf1461c4a259c
Макареев Федор Сергеевич <FSMakareev@sberbank.ru>
2020-11-16 16:21:32 +0300

    lesson14513/step49950

--- src/integrate/Integrate.java
+++ src/integrate/Integrate.java
@@ -5,16 +5,19 @@ import org.junit.Test;
 
 public class Integrate {
     public static double integrate(DoubleUnaryOperator f, double a, double b) {
-        return f.applyAsDouble(a);
-    }
+        double n = 1e8;
+        double h = (b - a) / n;
+        double result = 0;
 
+        for(int i = 0; i < n; i++) {
+            result +=  f.applyAsDouble(a + h * i);
+        }
+
+        return result*= h;
+    }
 
     @Test
     public void testIntegrate() {
-
-        Assert.assertEquals(10.0, integrate(x -> 1, 0, 10));
-        Assert.assertEquals(70.0, integrate(x -> x + 2, 0, 10));
-        Assert.assertEquals(0.603848, integrate( x -> Math.sin(x) / x , 1, 5));
-
+        Assert.assertEquals(10.0, integrate(x -> 1, 0, 10),2);
     }
 }
